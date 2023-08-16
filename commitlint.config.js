93ac807a1a36b9d9a3c80cb60d4f4398cf3adf4c
tremorScript <tremorscript@gmail.com>
2023-08-16 11:32:57 +0530

    feat(build): added commitlint and gitconfig
    docs: updated the commit message guidelinets

---
+++ commitlint.config.js
@@ -0,0 +1,4 @@
+module.exports = {extends: ['@commitlint/config-conventional']}
+
+//https://github.com/conventional-changelog/commitlint/blob/master/@commitlint/config-conventional/index.js
+//https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-angular
